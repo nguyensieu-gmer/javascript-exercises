d835003f9e5558d301f6039adf4ca39bd32874bc
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-02-06 17:00:23 +0700

    add something

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    let newSentence = "";
+    for (let char of sentence){
+        if (char.toUpperCase() !== char.toLowerCase() || (+char)){
+            newSentence += char.toLowerCase();
+        }
+    }
+    for (let i = 0; i < newSentence.length / 2; i++){
+        if (newSentence[i] !== newSentence[newSentence.length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
 };
+
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
